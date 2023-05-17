b72ba83a2086ecc1fb108cf87e7083e3af2906aa
Jan Balangue <janbalangue@Jans-MacBook-Air.local>
2023-05-17 15:41:38 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// Express application
+import express from 'express';
+
+import { fileURLToPath } from 'url';
+import { dirname, sep } from 'path';
+
+// configuration
+const
+    __dirname = dirname(fileURLToPath( import.meta.url )) + sep,
+    cfg = {
+        port: process.env.PORT || 3000,
+        dir: {
+            root: __dirname,
+            static: __dirname + 'static' + sep
+        }
+    };
+
+console.dir(cfg, { depth: null, color: true });
+    
+// Express initiation
+const app = express();
+
+app.use((req, res, next) => {
+    console.log(req.url);
+    next();
+}); 
+
+// home page route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// serve static assets
+app.use(express.static( cfg.dir.static ));
+
+// start server
+app.listen(cfg.port, () => {
+    console.log(`Example app listening at http://localhost:${ cfg.port }`);
+});
